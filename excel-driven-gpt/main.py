27c4629a7dbc0bcf51186312a2fb69d915c17386
Vitali Shulha <vitalliuss@gmail.com>
2024-09-02 18:22:26 +0400

    update excel sample

--- excel-driven-gpt/main.py
+++ excel-driven-gpt/main.py
@@ -9,12 +9,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-MODEL = "gpt-3.5-turbo-0613"
+MODEL = "gpt-3.5-turbo"
 DATA_FILE = 'data.xlsx'
 DATA_OUTPUT_XLSX = 'data_output.xlsx'
 
 def openai_call(system_message, input):
-    response = openai.ChatCompletion.create(
+    response = openai.chat.completions.create(
         model=MODEL,
         messages=[
             {"role": "system", "content": "" + system_message + ""},
@@ -22,7 +22,7 @@ def openai_call(system_message, input):
         ]
     )
     print(response)
-    result = response['choices'][0]['message']['content']
+    result = response.choices[0].message.content
     return result
 
 
@@ -32,7 +32,8 @@ def api_call_with_timeout(system_message, input, timeout_seconds, default_respon
     def make_api_call():
         try:
             response[0] = openai_call(system_message, input)
-        except:
+        except Exception as e:
+            print(e)
             pass
 
     thread = threading.Thread(target=make_api_call)
@@ -56,11 +57,13 @@ if __name__ == '__main__':
         try:
             call_count += 1
             print('Call #' + str(call_count))
-            ai_response = api_call_with_timeout(system_message, user_message, 30, "timeout reached") # seconds to wait for openai to respond
+            ai_response = api_call_with_timeout(system_message, user_message, 30, -1) # seconds to wait for openai to respond
         except Exception as e:
             ai_response = e
         # write content to dataframe column and row
         dataframe.at[index, 'ASSISTANT'] = ai_response
     dataframe.to_excel(DATA_OUTPUT_XLSX, index=False)
     time_elapsed = (datetime.datetime.now() - start_time).total_seconds()
-    print("Time spent, seconds: ", time_elapsed)
+    print('Time elapsed: ' + str(time_elapsed) + ' seconds')
+
+
