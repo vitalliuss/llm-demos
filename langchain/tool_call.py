6cc1d2461131ecd30af9b272fd7e438eeb1141c5
Vitali Shulha <vitalliuss@gmail.com>
2025-11-21 15:17:42 +0400

    fix tool call to github issues

--- langchain/tool_call.py
+++ langchain/tool_call.py
@@ -1,13 +1,17 @@
 import json
 import sqlite3
 import requests
+import os
 from langchain.chat_models import init_chat_model
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+
+load_dotenv()
+token = os.getenv("GITHUB_TOKEN")
 
 llm = init_chat_model("gpt-4o-mini", model_provider="openai")
-JIRA_API_URL= "https://your-company.atlassian.net/rest/api/2/issue/"
-JIRA_AUTH = ("your_email", "your_api_token")
 DB_PATH = "db/movies.sqlite"
 database_schema_string = """
 Table: movies
@@ -29,38 +33,52 @@ def ask_database(sql_query: str) -> str:
     except Exception as e:
         return str(e)
 
-
 @tool
-def create_support_ticket(summary: str, description: str) -> str:
-    """Creates a support ticket in Jira."""
-    payload = {
-        "fields": {
-            "project": {"key": "SUP"},
-            "summary": summary,
-            "description": description,
-            "issuetype": {"name": "Task"}
-        }
+def create_github_issue(title, body):
+    """
+    Creates an issue in the given GitHub repository and returns its title and URL.
+
+    :param title: Issue title
+    :param body: Issue body content
+    :return: Tuple (issue_title, issue_url) or None if creation failed
+    """
+    url = f"https://api.github.com/repos/vitalliuss/llm-demos/issues"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json"
     }
-    headers = {"Content-Type": "application/json"}
-    try:
-        response = requests.post(JIRA_API_URL, auth=JIRA_AUTH, json=payload, headers=headers)
-        return response.text
-    except Exception as e:
-        return str(e)
 
+    data = {
+        "title": title,
+        "body": body
+    }
 
-tools = [ask_database, create_support_ticket]
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 201:
+        issue_data = response.json()
+        issue_title = issue_data["title"]
+        issue_url = issue_data["html_url"]
+        return issue_title, issue_url
+    else:
+        print(f"Failed to create issue: {response.status_code}")
+        print(response.json())
+        return None
+
+tools = [ask_database, create_github_issue]
 
 llm_with_tools = llm.bind_tools(tools)
 
-query = "Can you create a support ticket for me? I want to add movies released in 2024."
+# query = "Can you create a support ticket for me? I want to add movies released in 2024."
+query = "I want to add new movies released in 2024 to the database. Can you help me with that?"
 system_message = SystemMessage('If the question is related to movies, use ask_database tool. It can execute SQL '
                                'against the SQLite database and return results. Here is the database schema: ' +
                                database_schema_string +
-                               'You can also create a support tickets using create_support_ticket tool if needed. '
+                               ' You can also create a support tickets using create_github_issue tool if needed. '
                                'Construct the summary and description of the ticket based on the previous user '
-                               'message and call the tool. Do not ask user for additional information. Resulting JIRA '
-                               'issue url should be returned with the following format: https://darkkernel.atlassian.net/browse/SUP-123')
+                               'message and call the tool. Do not ask user for additional information.  '
+                               )
 messages = [system_message,
             HumanMessage(query)]
 ai_msg = llm_with_tools.invoke(messages)
@@ -69,7 +87,7 @@ print(ai_msg)
 messages.append(ai_msg)
 
 for tool_call in ai_msg.tool_calls:
-    selected_tool = {"ask_database": ask_database, "create_support_ticket": create_support_ticket}[tool_call["name"].lower()]
+    selected_tool = {"ask_database": ask_database, "create_github_issue": create_github_issue}[tool_call["name"].lower()]
     tool_msg = selected_tool.invoke(tool_call)
     print('\n tool_msg', tool_msg)
     messages.append(tool_msg)
